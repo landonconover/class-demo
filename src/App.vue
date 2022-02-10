f2789bfdacfc0fd703bfe7b2ce550b665cb60d66
Landon Conover <landon.conover@gmail.com>
2022-02-09 17:22:15 -0700

    final

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>Shopping list</h1>
+  <ShoppingListForm @add:item='addItem' />
+  <ShoppingList 
+  @delete:item='deleteItem'
+  @edit:item='editItem'
+  :items='shoppingItems' />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ShoppingList from './components/ShoppingList.vue'
+import ShoppingListForm from './components/ShoppingListForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ShoppingList,
+    ShoppingListForm
+  },
+  data() {
+    return {
+      shoppingItems: [
+        {
+          id: 1,
+          name: 'Jelly',
+          amount: 1,
+        },
+        {
+          id: 2,
+          name: 'Peanut Butter',
+          amount: 3,
+        },
+        {
+          id: 3,
+          name: 'Bread',
+          amount: 1,
+        },
+        {
+          id: 4,
+          name: 'mustard',
+          amount: 1,
+        },
+      ]
+    }
+  },
+  methods: {
+    addItem(item) {
+      //make an id too
+      const lastId = this.shoppingItems.length > 0
+      ? this.shoppingItems[this.shoppingItems.length -1].id
+      : 0;
+
+      const id = lastId + 1;
+
+      const newItem = { ...item, id}
+
+      this.shoppingItems = [ ...this.shoppingItems, newItem]
+    },
+    deleteItem(id) {
+      this.shoppingItems = this.shoppingItems.filter(item => item.id !== id)
+    },
+    editItem(id, updatedItem) {
+      this.shoppingItems = this.shoppingItems
+        .map(item => {
+          return item.id === id ? updatedItem : item
+        })
+    }
   }
 }
 </script>
---
+++ src/components/ShoppingListForm.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="shopping-form">
+    <form @submit.prevent='handleSubmit'>
+      <label>Shopping Item</label>
+      <input v-model="item.name" type="text" />
+      <label>Item Amount</label>
+      <input v-model="item.amount" type="number" />
+      <button>Add item</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'shopping-form',
+    data() {
+      return {
+        item: {
+          name: '',
+          amount: '',
+        },
+      }
+    },
+    methods: {
+        handleSubmit() {
+            this.$emit('add:item', this.item)
+            this.item = {
+                name: '',
+                amount: '',
+            }
+        }
+    }
+}
+</script>
+
+<style>
+form {
+    margin-bottom: 2rem;
+  }
+</style>
